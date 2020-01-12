b981bfa701779038311fd71fee777494dd2ab3ab
hossein tabatabaei <hosseintabatabaei@yahoo.com>
2020-01-12 15:36:28 +0330

    Updates

---
+++ precache-manifest.65308a2091a1b50c5db47d1a8e1732c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32800242c615b90f7656a1c1c32f5d5",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "f2f36a94710594f9d24c",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f2f36a94710594f9d24c",
+    "url": "/contactmanager/static/js/2.a9e2844d.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.a9e2844d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ef84b16c15e4daa69bf",
+    "url": "/contactmanager/static/js/main.6e13796a.chunk.js"
+  },
+  {
+    "revision": "6d5470f28a9dc2aa0ea3",
+    "url": "/contactmanager/static/js/runtime-main.fef8aa86.js"
+  }
+]);
